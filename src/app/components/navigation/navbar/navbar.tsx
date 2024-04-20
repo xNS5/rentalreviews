c66025297b82c605db34f2edd0e1de369312407e
Michael Kennedy <michael@michaelkennedy.dev>
2024-04-19 18:50:47 -0700

    Trying things

--- src/app/components/navigation/navbar/navbar.tsx
+++ src/app/components/navigation/navbar/navbar.tsx
@@ -2,15 +2,21 @@
 
 import React from "react";
 import { usePathname } from 'next/navigation'
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button,   Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem} from "@nextui-org/react";
 import { Logo } from "./logo";
 
 const nav_items = [
-  {name: "Home", url: "/"},
-  {name: "Reviews", url: "/reviews"},
-  {name: "Resources", url: "/resources"},
-  {name: "About", url: "/about"},
-  {name: "Contact", url: "/contact"}
+  {name: "Home", url: "/", children: []},
+  {name: "Reviews", url: "/reviews", children: []},
+  {name: "Resources", url: "/resources", children: [
+    {name: "WA Tenants Union", url: "https://tenantsunion.org/"},
+  ]},
+  {name: "About", url: "/about", children: []},
+  {name: "Contact", url: "/contact", children: []}
 ]
 
 export const Nav = () => {
@@ -23,12 +29,13 @@ export const Nav = () => {
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
             {nav_items.map(x =>
-                <NavbarItem key={x.name} isActive={pathname == x.url}>
-                    <Link href={x.url} color="foreground">
-                        {x.name}
-                    </Link>
-                </NavbarItem>
-)}
+              <NavbarItem key={x.name} isActive={pathname == x.url}>
+              <Link href={x.url} color="foreground">
+                  {x.name}
+              </Link>
+          </NavbarItem>
+               
+        )}
         </NavbarContent>
     </Navbar>
   );
