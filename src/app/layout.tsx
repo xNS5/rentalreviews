dc75c030b569ad22710c32ff487b12edb1def556
Michael Kennedy <michael@michaelkennedy.dev>
2024-05-02 20:14:31 -0700

    Added static import for metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import { use } from "react";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
 import "./globals.css";
+import { getMetadata } from "./utilities/config-provider";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = getMetadata()
 
 export default function RootLayout({
   children,
--- src/app/utilities/config-provider.tsx
+++ src/app/utilities/config-provider.tsx
@@ -1,22 +1,23 @@
 import { promises as fs } from 'fs';
 import nav from "@/app/static/config/nav.json";
+import metadata from "@/app/static/config/metadata.json"
 
 const configPath = "../static/config";
 
 
 export async function getConfigForPage(filePath: string) {
     const res = await fetch(`${configPath}/${filePath}.json`);
+    console.log("RES: ", res);
     const data = JSON.parse(await res.json());
     return data;
 }
 
-export function getNavbarConfig() {
-    const navJson = JSON.parse(JSON.stringify(nav));
-    console.log(navJson);
-    return navJson;
+export function getMetadata(){
+    const metadataJson = JSON.parse(JSON.stringify(metadata));
+    return metadataJson;
 }
 
-export async function getMetadata(){
-    const ret = await getConfigForPage("metadata");
-    return ret;
+export function getNavbarConfig() {
+    const navJson = JSON.parse(JSON.stringify(nav));
+    return navJson;
 }
