06a69429a2aa3de720c59b2a947bd34bfee5f381
Michael Kennedy <michael@michaelkennedy.dev>
2024-05-06 20:17:17 -0700

    Adding more data to footer.json file, created getter for footer data

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,5 +1,21 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { getFooter } from "@/app/utilities/config-provider"
+import type { FooterItem } from "./footertypes"
 
 
-export const Footer = () => {
-    
+export const Footer = (data : FooterItem) => {
+    const [footerItems, setFooterItems] = useState({});
+
+    useEffect(() => {
+        const data = getFooter();
+        setFooterItems(data);
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    )
 }
--- src/app/components/footer/footertypes.tsx
+++ src/app/components/footer/footertypes.tsx
@@ -5,5 +5,7 @@ export type FooterItem = {
     icon: string | undefined,
     url: string | undefined,
     target: string | undefined,
-    text: string | undefined
+    text: string | undefined,
+    title: string | undefined,
+    alt: string | undefined
 }
