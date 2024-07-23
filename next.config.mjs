c22a07c32cf131b4a0229fe6c9d73e528a16bd3e
Michael Kennedy <michael@michaelkennedy.dev>
2024-07-22 18:12:07 -0700

    Adding facebook as an exception to remote image whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
         remotePatterns: [
             {
                 hostname: "**.githubusercontent.com",
+            },
+            {
+                hostname: "**.fbcdn.net"
             }
         ]
     }
