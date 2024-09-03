04255d5ea543a41310a1246bbfb55b665fb256c1
Michael Kennedy <michael@michaelkennedy.dev>
2024-09-03 14:25:46 -0700

    Re-adding REST API, but a slimmed down version of it. Moving getCompanyData() outside of review page to @/lib so it's independent of the page itself. Switching the getDocument/getCollection functions to a standard declaration just cuz

--- src/app/reviews/[slug]/page.tsx
+++ src/app/reviews/[slug]/page.tsx
@@ -1,32 +1,9 @@
-import { development } from "@/lib/config-provider";
 import { notFound } from "next/navigation";
 import { Review } from "./review";
 import { Spinner } from "@/components/spinner/spinner";
 import { Suspense } from "react";
-import { getDocument } from "@/app/db/db";
+import { getCompanyData } from "@/lib/getCompanyData";
 import type { Company } from "../columns";
-
-
-export async function getCompanyData(slug: string): Promise<Company> {
-    const article: Company | undefined = await getDocument<Company>("articles", slug);
-    const review: Company | undefined = await getDocument<Company>("reviews", slug);
-    const company: Company | undefined = await getDocument<Company>("companies", slug);
-
-    if(article){
-        delete article[development? "_id": "id"];
-    }
-
-    if(review){
-        delete review[development? "_id": "id"];
-    }
-
-    return {
-        article: { ...(article ?? {}) },
-        reviews: { ...(review ?? {}) },
-        ...(company ?? {})
-    } as Company;
-}
-
 
 export default async function Page({ params }: Readonly<{
     params: { [key: string]: string }
