c860409ad394a2464959261ae6aa1abc3840e990
Michael Kennedy <michael@michaelkennedy.dev>
2024-09-09 10:42:22 -0700

    Switching raw data back to dedicated page instead of built-in JSON viewer

---
+++ src/app/reviews/[slug]/data/json-wrapper.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { JsonViewer } from 'view-json-react';
+
+
+export function JsonWrapper({data}: Readonly<{
+    data: { [key: string]: string }
+}>){
+    return <JsonViewer data={data} expandLevel={1}/>
+}
--- src/app/reviews/[slug]/page.tsx
+++ src/app/reviews/[slug]/page.tsx
@@ -4,14 +4,14 @@ import { Suspense } from "react";
 import getCompanyData from "@/lib/getCompanyData";
 import type { Company } from "../columns";
 import Loading from "@/app/loading";
+import { isValidSlug } from "@/lib/utils";
 
 export default async function Page({ params }: Readonly<{
     params: { [key: string]: string }
 }>) {
-    const slug_regex_test = new RegExp('[^0-9a-z-]')
     const { slug } = params;
 
-    if (slug == undefined || slug_regex_test.test(slug)) {
+    if (slug == undefined || !isValidSlug(slug)) {
         notFound();
     }
 
