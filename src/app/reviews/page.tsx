58bcf029e07fa3837dbd3cabda136e8d066906e0
Michael Kennedy <michael@michaelkennedy.dev>
2024-10-01 20:41:34 -0700

    Fixing issue with bad state hooks, creating dedicated hover state to display link icon

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -10,12 +10,12 @@ export const dynamic = 'force-dynamic';
 
 export default async function Reviews() {
   const data: Company[] | undefined = await getCollection<Company>("companies");
-  // const alt = await getAltObj("reviews");
+  const alt = await getAltObj("reviews");
 
   return (
     <div className="container mx-auto py-10">
       {/* <ReviewsTableWrapper data={data ?? []} alt={alt}/> */}
-      <DataTable data={data} columns={ColumnKeys} tableCaption={"Rental Reviews Data"}/>
+      <DataTable data={data} columns={ColumnKeys} alt={alt} tableCaption={"Rental Reviews Data"}/>
     </div>
   );
 }
--- src/components/aria-table/data-table.tsx
+++ src/components/aria-table/data-table.tsx
@@ -5,27 +5,31 @@ import { Table, TableHeader, TableBody, TableHead, Row, Cell, Caption, Column } 
 import Icon from "../icons/icon";
 import { Input } from "@/components/ui/input";
 import { Company, ColumnType } from "@/app/reviews/columns";
-import { useMemo, useState } from "react";
-import { SortDescriptor } from "react-stately";
+import { Fragment, useMemo, useState } from "react";
+import { SortDescriptor, SortDirection } from "react-stately";
 import { Button } from "../ui/button";
+import { AltRecord } from "@/lib/altprovider";
+import Link from "next/link";
 
 const DEFAULT_PAGINATION_VALUE = 10;
 
 export default function DataTable({
   columns,
   data,
+  alt,
   tableCaption,
   paginationValue = DEFAULT_PAGINATION_VALUE,
 }: Readonly<{
   columns: ColumnType[];
   data: Company[];
+  alt: AltRecord;
   tableCaption: string;
   paginationValue?: number;
 }>) {
-  if (!data) return;
-
   const rowKeys = ["name", "company_type", "average_rating", "adjusted_average_rating", "review_count"];
   const [currentPage, setCurrentPage] = useState(1);
+  const [hoverStates, setHoverStates] = useState<{[key: string]: boolean}>({});
+  const pageCount = Math.ceil(data.length / paginationValue);
 
   let [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
     column: "name",
@@ -38,9 +42,9 @@ export default function DataTable({
       let second = b[sortDescriptor.column as keyof Company];
 
       if (typeof first === "number" && typeof second === "number") {
-        if(sortDescriptor.direction === "descending"){
+        if (sortDescriptor.direction === "descending") {
           return second - first;
-        } 
+        }
         return first - second;
       }
       let cmp = first.localeCompare(second);
@@ -50,56 +54,116 @@ export default function DataTable({
       return cmp;
     });
   }, [sortDescriptor]);
-  
+
   const currentPageData = useMemo(() => {
-    return sortedItems.slice(
-      (currentPage - 1) * paginationValue,
-      currentPage * paginationValue
-    )
-  }, [sortedItems, currentPage]) 
+    if (currentPage === pageCount) {
+      return sortedItems.slice((currentPage - 1) * paginationValue);
+    }
+    return sortedItems.slice((currentPage - 1) * paginationValue, currentPage * paginationValue);
+  }, [sortedItems, currentPage, sortDescriptor]);
 
 
+  const handleMouseEnter = (key: any) => {
+    setHoverStates((prev) => ({ ...prev, [key]: true }));
+  };
 
-  
+  const handleMouseLeave = (key: any) => {
+    setHoverStates((prev) => ({ ...prev, [key]: false }));
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    setHoverStates({});
+  }
 
   return (
     <div className="relative w-full overflow-auto border-2 border-solid border-slate-500 rounded-lg">
       <Table aria-label={tableCaption} sortDescriptor={sortDescriptor} onSortChange={setSortDescriptor}>
         <TableHeader>
           {columns.map((column, i: number) => (
-            <Column id={column.key} key={i} {...(i === 0 ? { isRowHeader: true } : undefined)} sortDescriptor={sortDescriptor} allowsSorting className={`border-black ${i < columns.length - 1 ? "border-r-1" : ""}`}>
+            <Column
+              id={column.key}
+              key={i}
+              isRowHeader={i === 0}
+              sortDescriptor={sortDescriptor}
+              allowsSorting
+              className={`border-black ${i < columns.length - 1 ? "border-r-1" : ""}`}
+            >
               {column.title}
             </Column>
           ))}
         </TableHeader>
         <TableBody>
-          {currentPageData.map((item, i: number) => (
-            <Row key={i}>
-              {rowKeys.map((key: string, j: number) => (
-                <Cell key={j} className={`${j < rowKeys.length - 1 ? "border-black border-r-1" : ""}`}>{`${item[key]}${
-                  key.includes("rating") ? "/5" : ""
-                }`}</Cell>
-              ))}
-            </Row>
-          ))}
+          {currentPageData.map((item, i: number) => {
+            const currRow: React.ReactNode[] = rowKeys.map((key: string, j: number) => {
+              const className = j < rowKeys.length ? "border-black border-r-1" : "";
+              const isHover = hoverStates[item.slug];
+              return (
+                <Cell key={j} className={`${className}`}>
+                  {j == 0 ? (
+                  <Link
+                    id={`${key}-link`}
+                    href={`/reviews/${item.slug}`}
+                    className={`flex mx-3 font-medium items-center justify-center`}
+                    onMouseEnter={() => handleMouseEnter(item.slug)}
+                    onMouseLeave={() =>  handleMouseLeave(item.slug)}
+                  >
+                    {`${item[key]}`}
+                    <Icon type="fas-link" ariahidden={true} className={`${hoverStates[item.slug] ? "visible" : "invisible" } mx-1 h-4 w-4`} />
+                  </Link>
+
+                  ): (
+                    <>{`${item[key]}${key.includes("rating") ? "/5" : ""}`}</>
+                  )}
+                </Cell>
+              );
+            });
+
+            return <Row key={i}>{currRow}</Row>;
+          })}
         </TableBody>
       </Table>
       <span className="py-2 flex flex-col justify-center text-center">
         <span className="flex flex-row justify-center text-center">
-        <Button variant={"ghost"}>
-          {"<<"}
-        </Button>
-        <Button variant={"ghost"}>
-          {"<"}
-        </Button>
-        <Button variant={"ghost"}>
-          {">"}
-        </Button>
-        <Button variant={"ghost"}>
-          {">>"}
-        </Button>
+          <Button 
+          variant={"ghost"} 
+          aria-label="last page" 
+          disabled={currentPage === 1} 
+          aria-disabled={currentPage === 1} 
+          onClick={() => handlePageChange(1)}>
+            {"<<"}
+          </Button>
+          <Button
+            variant={"ghost"}
+            aria-label="previous page"
+            disabled={currentPage === 1}
+            aria-disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            {"<"}
+          </Button>
+          <Button
+            variant={"ghost"}
+            aria-label="next page"
+            disabled={currentPage === pageCount}
+            aria-disabled={currentPage === pageCount}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            {">"}
+          </Button>
+          <Button
+            variant={"ghost"}
+            aria-label="last page"
+            disabled={currentPage === pageCount}
+            aria-disabled={currentPage === pageCount}
+            onClick={() => handlePageChange(pageCount)}
+          >
+            {">>"}
+          </Button>
         </span>
-        <p>Page {currentPage} of {Math.ceil(sortedItems.length / paginationValue)}</p>
+        <p>
+          Page {currentPage} of {pageCount}
+        </p>
       </span>
     </div>
   );
