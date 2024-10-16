3b4aa7398a32b66160e0b824c2bceb91ecc9e1fc
Michael Kennedy <michael@michaelkennedy.dev>
2024-10-15 20:17:17 -0700

    Tweaking tailwindcss breakpoints, removing dedicated mobile view for data list

--- src/app/reviews/[slug]/review.tsx
+++ src/app/reviews/[slug]/review.tsx
@@ -5,6 +5,7 @@ import Text from "@/components/text/text";
 import Accordion from "@/components/accordion/accordion";
 import { AltRecord, getAltString } from "@/lib/altprovider";
 import "./review.css";
+import Icon from "@/components/icons/icon";
 
 const adjustedReviewDisclaimerString = "Note: The Adjusted Review Count and Rating reflect only reviews with both text and a rating.";
 
@@ -14,19 +15,19 @@ export function Review({data, altObj}: Readonly<{data: Company, altObj: AltRecor
 
   return (
     <Article className="container mx-auto py-10 review-summary">
-      <h1 className="text-2xl md:text-3xl text-center font-bold my-1">{data.name}</h1>
-      <div id="review-data-list" className="hidden md:flex flex-col items-center justify-center">
-        <ol className="flex flex-row lg:text-lg sm:text-sm ">
+      <h1 className="text-3xl md:text-2xl text-center font-bold my-1">{data.name}</h1>
+      <div id="review-data-list" className="container flex flex-col items-center justify-center border md:border-none max-w-md md:max-w-none border-slate-500 rounded">
+         <ol className="block md:flex md:flex-row  text-xl md:text-lg  whitespace-nowrap space-y-3 md:space-y-0 text-left last:ml-auto my-3">
           <li>
             <label className="sr-only">{getAltString(altObj, "review_count", data.review_count)}</label>
-            <p className="m-1" aria-hidden={true}>
+            <p className="mx-1" aria-hidden={true}>
               <b>Review Count: </b>
               {data.review_count}
             </p>
           </li>
           <li>
             <label className="sr-only">{getAltString(altObj, "average_rating", data.average_rating)}</label>
-            <p className="m-1" aria-hidden={true}>
+            <p className="mx-1" aria-hidden={true}>
               <b>Average Rating: </b>
               {data.average_rating}/5
             </p>
@@ -35,86 +36,32 @@ export function Review({data, altObj}: Readonly<{data: Company, altObj: AltRecor
             <>
               <li>
                 <label className="sr-only">{getAltString(altObj, "adjusted_review_count", data.adjusted_review_count)}</label>
-                <p className="m-1" aria-hidden={true}>
+                <p className="mx-1" aria-hidden={true}>
                   <b>Adjusted Review Count: </b>
                   {data.adjusted_review_count}
                 </p>
               </li>
               <li>
                 <label className="sr-only">{getAltString(altObj, "adjusted_average_rating", data.adjusted_average_rating)}</label>
-                <p className="m-1" aria-hidden={true}>
+                <p className="mx-1" aria-hidden={true}>
                   <b>Adjusted Rating: </b>
                   {data.adjusted_average_rating}/5
                 </p>
               </li>
             </>
           )}
-          <li>
-            <Link href={`/api?id=${data.slug}`} className="rounded flex text-center justify-center mx-3 bg-blue-500 h-8 px-2 w-auto text-white">
-              Raw Data
+          <li className="grow">
+            <Link href={`/api?id=${data.slug}`} className="rounded flex text-center items-center justify-center mx-3 bg-blue-600 text-white h-8 w-auto">
+            <p className="py-2 pl-4 pr-1 !text-base">Raw Data</p>
+              <Icon type="fas-link" ariahidden={true} className={`pr-4 h-4 w-4`} />
             </Link>
           </li>
         </ol>
-        {hasAdjustedReviewValue && <p className="text-sm lg:text-lg" aria-hidden="true">{adjustedReviewDisclaimerString}</p>}
+        {hasAdjustedReviewValue && <p className="text-sm lg:text-lg mb-2" aria-hidden="true">{adjustedReviewDisclaimerString}</p>}
       </div>
       {/* Divider element */}
       <div className="hidden md:flex relativepb-5 pt-2 items-center">
         <div className="flex-grow border-t border-gray-400"></div>
-      </div>
-
-      {/* Mobile component */}
-      <div id="data-accordion" className="flex md:hidden w-full flex-col items-center">
-        <Accordion
-          triggerText={"Data"}
-          defaultValue={"Data"}
-          className={{
-            trigger: "font-semibold inline-flex justify-center",
-            comp: "border border-gray-400 border-1 rounded w-full p-2 shadow-md",
-            content: "p-5 grid-cols-1",
-          }}
-        >
-          <ol className="space-y-2 text-gray-500 list-none list-inside dark:text-gray-400 text-lg !text-black">
-          <li>
-            <label className="sr-only">{getAltString(altObj, "review_count", data.review_count)}</label>
-            <p className="m-1" aria-hidden={true}>
-              <b>Review Count: </b>
-              {data.review_count}
-            </p>
-          </li>
-          <li>
-            <label className="sr-only">{getAltString(altObj, "average_rating", data.average_rating)}</label>
-            <p className="m-1" aria-hidden={true}>
-              <b>Average Rating: </b>
-              {data.average_rating}/5
-            </p>
-          </li>
-          {hasAdjustedReviewValue && (
-            <>
-              <li>
-                <label className="sr-only">{getAltString(altObj, "adjusted_review_count", data.adjusted_review_count)}</label>
-                <p className="m-1" aria-hidden={true}>
-                  <b>Adjusted Review Count: </b>
-                  {data.adjusted_review_count}
-                </p>
-              </li>
-              <li>
-                <label className="sr-only">{getAltString(altObj, "adjusted_average_rating", data.adjusted_average_rating)}</label>
-                <p className="m-1" aria-hidden={true}>
-                  <b>Adjusted Rating: </b>
-                  {data.adjusted_average_rating}/5
-                </p>
-              </li>
-            </>
-          )}
-          </ol>
-          <Link
-            href={`/reviews/${data.slug}/data`}
-            className="flex text-lg h-8 my-2 w-auto items-center justify-center rounded mx-1 bg-blue-500 px-2 text-white"
-          >
-            Raw Data
-          </Link>
-          {hasAdjustedReviewValue && <span className="flex justify-center text-md">{adjustedReviewDisclaimerString}</span>}
-        </Accordion>
       </div>
 
       {/* Conditionally display disclaimer */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,10 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      screens: {
+        md: "940px",
+        xxs: "500px"
+      }
     },
   },
 } satisfies Config
