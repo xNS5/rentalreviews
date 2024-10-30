35aab9ed6c839f8bd806fd465e1ae7900f361d61
Michael Kennedy <michael@michaelkennedy.dev>
2024-10-29 23:07:04 -0700

    Fixing some oddities with the navbar

--- src/components/navigation-menu/navigation-menu.tsx
+++ src/components/navigation-menu/navigation-menu.tsx
@@ -38,11 +38,11 @@ export default function NavigationMenu({
           {data.name}
           <Icon
             type="fas-chevron-down"
-            className={`mt-2 h-3 w-3 ml-1 text-inherit transition-transform ${isNavOpen ? "rotate-180 transform" : ""}`}
+            className={`h-4 w-4 ml-1 text-inherit transition-transform ${isNavOpen ? "rotate-180 transform" : ""}`}
           />
         </Button>
       </DropdownTrigger>
-      <DropdownMenu variant="flat" className={`${className?.menu ?? ""}`} items={data.children} {...(props?.menu && props.menu)}>
+      <DropdownMenu variant="flat" className={`${className?.menu ?? ""} bg-white border border-slate-200 shadow rounded p-2`} items={data.children} {...(props?.menu && props.menu)}>
         {(child: LinkType) => (
           <DropdownItem key={`${child.name}`} value={child.name} variant="flat" className={`${className?.item ?? ""}`}>
             <Link href={child.url} onClick={() => setIsNavOpen(!isNavOpen)} target={child.target}>
