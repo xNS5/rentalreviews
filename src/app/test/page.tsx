ba511911968244143a6184a5acee99e24b3ca183
Michael Kennedy <michael@michaelkennedy.dev>
2024-12-02 20:55:30 -0800

    Deleting dedicated component in /ui/ to not over-complicate things, have barebones solution

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,25 @@
-import Popover from "@/components/table-filter-popover/popover";
+"use client"
 
-export default function TestPage(){
+import React, {useContext} from "react";
+import {Config, ConfigContext} from "@/lib/configProvider";
+import Popover from "@/components/table-filter-popover/popover";
+import {Icon} from "@/components/icon/icon";
+
+type FilterProps = {
+    title: string,
+    key: string,
+    type: string,
+    value: any
+}
+
+export default async function TestPage(){
+    const {reviews}: Config = useContext(ConfigContext);
+    const {filter_props} = reviews;
+
     return (
-        <Popover/>
+        <Popover toggle={<>Filter <Icon className={"h-4 w-4 px-1"} type={'fas-filter'}/></> }>
+            {filter_props.map((prop: FilterProps) => <p key={Math.random()}>{prop.title}</p>)}
+        </Popover>
     )
 
 }
--- src/components/table-filter-popover/popover.tsx
+++ src/components/table-filter-popover/popover.tsx
@@ -1,109 +1,21 @@
 "use client";
 
-/*
- * Company Type: Select
- * Average Rating: Select
- * Adjusted Average Rating: Select
- * Review Count: Select
- * E.g. "Average Rating: 3+"
- * */
-
 import React, { useRef, useState, useEffect } from "react";
 
-const FilterProps = [
-  {
-    title: "Company Type",
-    key: "company_type",
-    type: "select",
-    value: [
-      { title: "Company", key: "company" },
-      { title: "Property", key: "property" },
-    ],
-  },
-  {
-    title: "Average Rating",
-    key: "average_rating",
-    type: "select",
-    value: [
-      {
-        title: 1,
-        key: 1,
-      },
-      {
-        title: 2,
-        key: 2,
-      },
-      {
-        title: 3,
-        key: 3,
-      },
-      {
-        title: 4,
-        key: 4,
-      },
-      {
-        title: 5,
-        key: 5,
-      },
-    ],
-  },
-  {
-    title: "Adjusted Average Rating",
-    key: "adjusted_average_rating",
-    type: "select",
-    value: [
-      {
-        title: 1,
-        key: 1,
-      },
-      {
-        title: 2,
-        key: 2,
-      },
-      {
-        title: 3,
-        key: 3,
-      },
-      {
-        title: 4,
-        key: 4,
-      },
-      {
-        title: 5,
-        key: 5,
-      },
-    ],
-  },
-  {
-    title: "Review Count",
-    key: "review_count",
-    type: "select",
-    value: [
-      {
-        title: 10,
-        key: 10,
-      },
-      {
-        title: 20,
-        key: 20,
-      },
-      {
-        title: 30,
-        key: 30,
-      },
-      {
-        title: 40,
-        key: 40,
-      },
-      {
-        title: 50,
-        key: 50,
-      },
-    ],
-  },
-];
-
-export default function Popover() {
+export default function Popover({
+  children,
+  toggle,
+  className,
+  props,
+}: Readonly<{
+  children: React.ReactNode;
+  toggle: React.ReactNode;
+  className?: {
+    toggle: string;
+    popover: string;
+  };
+  [key: string]: any;
+}>) {
   const [isVisible, setIsVisible] = useState(false);
   const popoverRef = useRef<HTMLDivElement>(null);
   const triggerRef = useRef<HTMLButtonElement>(null);
@@ -114,10 +26,11 @@ export default function Popover() {
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (event.target &&
+      if (
+        event.target &&
         popoverRef.current &&
         triggerRef.current &&
-      !popoverRef.current.contains(event.target as Node) &&
+        !popoverRef.current.contains(event.target as Node) &&
         !triggerRef.current.contains(event.target as Node)
       ) {
         popoverVisibilityHandler();
@@ -131,26 +44,29 @@ export default function Popover() {
   }, []);
 
   return (
-    <div className="relative flex flex-row justify-center">
+    <div
+      className={`relative flex flex-row justify-center ${className?.popover ?? ""}`}
+      {...props}
+    >
       <button
         ref={triggerRef}
         onClick={popoverVisibilityHandler}
-        className="cursor-pointer rounded pt-2 "
+        className={`flex flex-row cursor-pointer rounded pt-2 ${className?.toggle ?? ""}`}
         aria-haspopup="true"
         aria-expanded={isVisible}
         aria-controls="popover-content"
       >
-        Filters
+        {toggle}
       </button>
       {isVisible && (
         <div
           id="popover-content"
           ref={popoverRef}
-          className="flex flex-col bg-white border border-black absolute top-full left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 shadow-md rounded-md p-4 z-[1000] whitespace-nowrap"
+          className="flex flex-col bg-white border border-black absolute animate ease-in-out top-full left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 shadow-md rounded-md p-4 z-[1000] whitespace-nowrap"
           role="dialog"
           aria-modal="true"
         >
-          {FilterProps.map(prop => <div key={Math.random()}>{prop.title}</div>)}
+          {children}
         </div>
       )}
     </div>
