ab1c885186ae465c2a40b7a78ae7b0712c4be96c
Michael Kennedy <michael@michaelkennedy.dev>
2024-12-15 16:56:05 -0800

    Creating basic page component for text-only pages (e.g. privacy policy, accessibility), migrating individual CSS files to basicPage.css

--- src/app/accessibility/page.tsx
+++ src/app/accessibility/page.tsx
@@ -1,25 +1,8 @@
-"use client"
-
-import React, {useContext} from "react";
-import Text from "@/components/text/text";
-import Article from "@/components/article/article";
-import {Config, ConfigContext} from "@/lib/configProvider";
-import {notFound} from "next/navigation";
-
-import "./accessibility.css";
+import BasicPage from "@/components/basic-page/basicPage";
 
 export default function Page() {
-    const { accessibility }: Config = useContext(ConfigContext);
-
-    if(accessibility === undefined){
-        console.error("Data is undefined. Check DB connection.");
-        notFound();
-    }
-
     return (
-        <Article className="container" announcement={accessibility.aria_announcement ?? undefined}>
-            <Text text={accessibility?.text ?? ""} />
-        </Article>
+        <BasicPage configName={"accessibility"}/>
     );
 }
 
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,26 +1,8 @@
-"use client"
-
-import {useContext} from "react";
-import Text from "@/components/text/text";
-import Article from "@/components/article/article";
-import {Config, ConfigContext} from "@/lib/configProvider";
-import {notFound} from "next/navigation";
-
-import "./privacy_policy.css";
+import BasicPage from "@/components/basic-page/basicPage";
 
 export default function Page() {
-  const { privacy_policy }: Config = useContext(ConfigContext);
-
-  if(privacy_policy === undefined){
-      console.error("Data is undefined. Check DB connection.");
-      notFound();
-  }
-
-
-  return (
-    <Article className="container" announcement={privacy_policy.aria_announcement ?? undefined}>
-      <Text text={privacy_policy?.text ?? ""} />
-    </Article>
-  );
+    return (
+        <BasicPage configName={"privacy_policy"}/>
+    );
 }
 
