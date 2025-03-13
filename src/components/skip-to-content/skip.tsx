53ce2158ccb4f3b18701046c7a35c104deb802c0
Michael Kennedy <michael@michaelkennedy.dev>
2025-03-13 10:24:41 -0700

    Fixing skip to link

--- src/components/skip-to-content/skip.tsx
+++ src/components/skip-to-content/skip.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function SkipToContent({
   className,
-    href,
+  href,
   ...rest
 }: Readonly<{
   href: string
@@ -15,7 +15,7 @@ export default function SkipToContent({
   return (
     <Link
     id="skip-link"
-    href={"/"}
+    href={href ?? "/"}
     className={`transition left-0 bg-primary text-primary-content absolute p-3 m-3 -translate-y-16 focus:translate-y-0 ${className}`}
     {...rest}
     >
